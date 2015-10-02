95bfac85cc30fbcc6f6e6235b0c8a4dabae8454e
razvan-tudosa <tudosa.razvan@gmail.com>
2015-10-02 09:44:50 +0300

    - finishes work on homepage (social media section) - adds behaviour: find more buttons hides on last slide - starts work on artists page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
+
+  var pages = ['acasa', 'despreFestival', 'artisti', 'program', 'parteneri'];
+
   $('#fullpage').fullpage({
-    anchors: ['acasa', 'despreFestival', 'artisti', 'program', 'parteneri'],
-    menu: '#menu'
+    anchors: pages,
+    menu: '#menu',
+    onLeave: onLeaveHandler
   });
 
   $('.next-page').on('click', function(event) {
     $.fn.fullpage.moveSectionDown();
   });
+
+  function onLeaveHandler(index, nextIndex, direction) {
+    if (pages.length === nextIndex) {
+      // last page hide the 'next-page button'
+      $('.next-page').addClass('go-away');
+    }
+
+    if (index === pages.length) {
+      $('.next-page').removeClass('go-away');
+    }
+  }
 });
